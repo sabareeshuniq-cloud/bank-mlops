a14a2ae5fbd08e0d68826f32b1502166fd1c71c1
sabareeshuniq-cloud <sabareeshuniq@gmail.com>
2026-02-17 16:30:04 +0530

    Update params.py

--- configs/params.py
+++ configs/params.py
@@ -1,5 +1,15 @@
 import yaml
+import os
 
-def load_config(env="dev"):
-    with open(f"configs/{env}.yaml") as f:
-        return yaml.safe_load(f)
+ENV = os.environ.get("ENV", "dev")
+
+with open(f"configs/{ENV}.yaml") as f:
+    config = yaml.safe_load(f)
+
+PROJECT = config["project"]
+REGION = config["region"]
+TRAIN_INSTANCE = config["training_instance"]
+PROCESS_INSTANCE = config["processing_instance"]
+ENDPOINT_INSTANCE = config["endpoint_instance"]
+ACCURACY_THRESHOLD = config["min_accuracy"]
+BUCKET_PREFIX = config["bucket_prefix"]
